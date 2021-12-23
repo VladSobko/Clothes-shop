67a93682df143632357843198e06d577681baa28
DESKTOP-40F2KVA\user <katherina0799@gmail.com>
2021-12-23 19:32:32 +0200

    Kind, Category and Sex models have been added
    With the help of these models it is possible to combine certain elements into categories.

---
+++ shop_proj/Models/Kind.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_proj.Models
+{
+    public class Kind
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Введіть колір")]
+        public string Colour { get; set; }
+        public Tovar Tovar { get; set; }
+        public int TovarId { get; set; }
+        public List<Size> Sizes { get; set; }
+        public List<Image> Images { get; set; }
+    
+    }
+}
