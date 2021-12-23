4ea29644945f14981b157240df220f9bb1a7a7c9
DESKTOP-40F2KVA\user <katherina0799@gmail.com>
2021-12-23 19:29:28 +0200

    Komentar and Answer models have been added.
    With the help of these models it is possible to write comments and answers to them.

---
+++ shop_proj/Models/Komentar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_proj.Models
+{
+    public class Komentar
+    {
+        public int Id { get; set; }
+        public Tovar Tovar { get; set; }
+        public int TovarId { get; set; }
+        public User User { get; set; }
+        public string UserId { get; set; }
+        public string Text { get; set; }
+        public DateTime date { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
