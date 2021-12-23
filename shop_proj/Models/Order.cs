f6c4caf70ac70875293d1b2f48e949d23b4facfc
DESKTOP-40F2KVA\user <katherina0799@gmail.com>
2021-12-23 19:26:38 +0200

    Order and OrderItem models have been added
    These models are responsible for accounting orders.

---
+++ shop_proj/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_proj.Models
+{
+    public class Order
+    { 
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string UserId { get; set; }
+        public List<Orderitem> Items { get; set; }
+        public DateTime datezam { get; set; }
+        public float Price { get; set; }
+        public string Delivery { get; set; }
+        public string City { get; set; }
+        public string address { get; set; }
+        public string postoffice { get; set; }
+        public string Card_num { get; set; }
+
+        public string Card_date { get; set; }
+         public string Card_cvv { get; set; }
+
+    }
+}
