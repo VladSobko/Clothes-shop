fcf998aa54df4a0da57a790dafb07bf53bb815db
DESKTOP-40F2KVA\user <katherina0799@gmail.com>
2021-12-23 19:14:17 +0200

    Tovar and Size models have been added
    These models are responsible for accounting for goods.

---
+++ shop_proj/Models/Size.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_proj.Models
+{
+    public class Size
+    {
+        public int Id { get; set; }
+        public Kind Kind { get; set; }
+        public int KindId { get; set; }
+         public String Name { get; set; }
+        public int Count { get; set; }
+        public Size(String name, int size)
+        {
+            this.Name = name;
+            this.Count = size;
+        }
+        public Size()
+        {
+        }
+    }
+
+}
---
+++ shop_proj/Models/Tovar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_proj.Models
+{
+    public class Tovar
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введіть назву")]
+        [StringLength(300, MinimumLength = 3, ErrorMessage = "Довжина рядка повинна бути від 3 до 300 символів")]
+        public string Name { get; set; }
+        public int ModellId { get; set; }
+        public Modell Modell { get; set; }
+        [Required(ErrorMessage = "Введіть бренд")]
+        public string Brend { get; set; }
+        [Required(ErrorMessage = "Введіть матеріал")]
+        public string Material { get; set; }
+
+        [Required(ErrorMessage = "Введіть ціну")]
+        public double Price { get; set; }
+        public List<Kind> Kinds { get; set; }
+        public List<Komentar> Komentars { get; set; }
+
+    }
+    
+}
