154c1f8db3a51c99deb13c18bb64fef4d235f2f0
DESKTOP-40F2KVA\user <katherina0799@gmail.com>
2021-12-23 19:17:57 +0200

    Korzyna and KorzynaItem models have been added
    These models are responsible for accounting for items in the cart.

---
+++ shop_proj/Models/Korzyna.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_proj.Models
+{
+    public class Korzyna
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string UserId { get; set; }
+        public List<Korzynaitem> Items { get; set; }
+
+       
+    }
+}
